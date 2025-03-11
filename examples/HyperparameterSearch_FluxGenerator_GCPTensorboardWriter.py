577cc02888b720ed07f499ab4f6b37ea8d340cae
Theresa <theresa.wohlsen@ottogroup.com>
2025-03-11 14:09:00 +0100

    TIGA-753: Feature/create flux fill generator (#72)
    * first working status
    
    * test for flux
    
    * added hyperparameter search
    
    * fix test
    
    * adapt path
    
    * small update
    
    * Linting
    
    * lint fix
    
    * cleaning up
    
    * small fixes
    
    * adjust test
    
    * Update pixaris/generation/flux.py
    
    * adjust example config
    
    ---------
    
    Co-authored-by: HenrikeMey <121957129+HenrikeMey@users.noreply.github.com>

---
+++ examples/HyperparameterSearch_FluxGenerator_GCPTensorboardWriter.py
@@ -0,0 +1,56 @@
+import yaml
+from pixaris.data_loaders.gcp import GCPDatasetLoader
+from pixaris.data_writers.gcp_tensorboard import GCPTensorboardWriter
+from pixaris.generation.flux import FluxFillGenerator
+from pixaris.orchestration.base import (
+    generate_images_for_hyperparameter_search_based_on_eval_set,
+)
+import os
+
+config = yaml.safe_load(open("pixaris/config.yaml", "r"))
+os.environ["BFL_API_KEY"] = config["bfl_api_key"]
+
+EVAL_SET = "test_eval_set"
+RUN_NAME = "example-flux"
+PROMPT_1 = "A beautiful woman in the desert"
+PROMPT_2 = "A beautiful woman on a moon"
+
+# +
+data_loader = GCPDatasetLoader(
+    gcp_project_id=config["gcp_project_id"],
+    gcp_bucket_name=config["gcp_bucket_name"],
+    eval_set=EVAL_SET,
+    eval_dir_local="eval_data",
+)
+
+generator = FluxFillGenerator()
+
+data_writer = GCPTensorboardWriter(
+    project_id=config["gcp_project_id"],
+    location=config["gcp_location"],
+    bucket_name=config["gcp_bucket_name"],
+)
+
+args = {
+    "hyperparameters": [
+        {
+            "node_name": "prompt",
+            "input": "prompt",
+            "value": [PROMPT_1, PROMPT_2],
+        },
+    ],
+    "eval_set": EVAL_SET,
+    "run_name": RUN_NAME,
+}
+# -
+
+# execute
+out = generate_images_for_hyperparameter_search_based_on_eval_set(
+    data_loader=data_loader,
+    image_generator=generator,
+    data_writer=data_writer,
+    metrics=[],
+    args=args,
+)
+
+out
---
+++ examples/LocalDatasetLoader_FluxGenerator_LocalDataWriter.py
@@ -0,0 +1,48 @@
+from pixaris.data_loaders.local import LocalDatasetLoader
+from pixaris.data_writers.local import LocalDataWriter
+from pixaris.generation.flux import FluxFillGenerator
+from pixaris.orchestration.base import generate_images_based_on_eval_set
+import os
+import yaml
+
+EVAL_SET = "mock"
+RUN_NAME = "example_flux"
+PROMPT = "A beautiful woman on a beach"
+
+# Set your API Key
+config = yaml.safe_load(open("pixaris/config.yaml", "r"))
+os.environ["BFL_API_KEY"] = config["bfl_api_key"]
+
+# +
+data_loader = LocalDatasetLoader(
+    eval_set=EVAL_SET,
+    eval_dir_local="test/test_eval_set",
+)
+
+generator = FluxFillGenerator()
+
+data_writer = LocalDataWriter()
+
+args = {
+    "generation_params": [
+        {
+            "node_name": "prompt",
+            "input": "prompt",
+            "value": PROMPT,
+        },
+    ],
+    "eval_set": EVAL_SET,
+    "run_name": RUN_NAME,
+}
+# -
+
+# execute
+out = generate_images_based_on_eval_set(
+    data_loader=data_loader,
+    image_generator=generator,
+    data_writer=data_writer,
+    metrics=[],
+    args=args,
+)
+
+out[0][0].show()
