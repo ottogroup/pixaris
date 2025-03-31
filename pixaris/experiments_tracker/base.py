51ad6674f3949e9fd6a790d3e67f4f6dd9abc12b
Theresa <theresa.wohlsen@ottogroup.com>
2025-03-31 15:09:41 +0200

    TIGA-831: Render experiments tab (#103)
    * render experiments tab
    
    * Add experiments_tracker and function to create_dummy_data_for_frontend
    
    ---------
    
    Co-authored-by: HenrikeMey <121957129+HenrikeMey@users.noreply.github.com>

---
+++ pixaris/experiments_tracker/base.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+
+
+class ExperimentTracker:
+    @abstractmethod
+    def load_projects_and_datasets(
+        self,
+        local_results_folder: str = "local_results",
+    ):
+        pass
+
+    @abstractmethod
+    def load_experiment_results_for_dataset(
+        self,
+        project: str,
+        dataset: str,
+        local_results_folder: str = "local_results",
+    ):
+        pass
