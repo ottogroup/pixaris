c90ede2f176d0ed84c31b315ab02a9afa9fc0277
Theresa <theresa.wohlsen@ottogroup.com>
2025-04-09 17:07:07 +0200

    Feature/tiga 863 imagen generator (#111)
    * wip
    
    * interim status
    
    * gitignore
    
    * Working version
    
    * remove
    
    ---------
    
    Co-authored-by: HenrikeMey <121957129+HenrikeMey@users.noreply.github.com>

---
+++ examples/LocalDatasetLoader_Imagen2Generator_LocalExperimentHandler.py
@@ -0,0 +1,53 @@
+from pixaris.data_loaders.local import LocalDatasetLoader
+from pixaris.experiment_handlers.local import LocalExperimentHandler
+from pixaris.generation.imagen2 import Imagen2ImageGenerator
+from pixaris.orchestration.base import generate_images_based_on_dataset
+import os
+import yaml
+import json
+from PIL import Image
+
+config = yaml.safe_load(open("pixaris/config.yaml", "r"))
+PROJECT = "dummy_project"
+DATASET = "dummy_dataset"
+with open(os.getcwd() + "/test/assets/test-background-generation.json", "r") as file:
+    WORKFLOW_APIFORMAT_JSON = json.load(file)
+WORKFLOW_PILLOW_IMAGE = Image.open(
+    os.getcwd() + "/test/assets/test-background-generation.png"
+)
+EXPERIMENT_RUN_NAME = "example-run"
+PROMPT = "A beautiful image of a moon"
+
+# +
+data_loader = LocalDatasetLoader(
+    project=PROJECT,
+    dataset=DATASET,
+    eval_dir_local="local_experiment_inputs",
+)
+
+generator = Imagen2ImageGenerator(
+    gcp_project_id=config["gcp_project_id"],
+    gcp_location=config["gcp_location"],
+)
+
+experiment_handler = LocalExperimentHandler()
+
+args = {
+    "workflow_apiformat_json": WORKFLOW_APIFORMAT_JSON,
+    "workflow_pillow_image": WORKFLOW_PILLOW_IMAGE,
+    "project": PROJECT,
+    "dataset": DATASET,
+    "experiment_run_name": EXPERIMENT_RUN_NAME,
+}
+# -
+
+# execute
+out = generate_images_based_on_dataset(
+    data_loader=data_loader,
+    image_generator=generator,
+    experiment_handler=experiment_handler,
+    metrics=[],
+    args=args,
+)
+
+out[0][0].show()
