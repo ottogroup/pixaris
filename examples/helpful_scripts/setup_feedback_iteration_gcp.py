ace6e63f25021a5be17f11e0e00b904a04b2b092
fideliusmanstein <fidelius.vonmanstein@ottogroup.com>
2025-04-14 16:25:06 +0200

    moved setup of feedback iterations into feedback handlers (#5)
    * moved setup of feedback iterations into feedback handlers
    
    * implemented all suggested changes
    
    * reverted experiment_run_name change
    
    * generic dummy variables
    
    ---------
    
    Co-authored-by: HenrikeMey <121957129+HenrikeMey@users.noreply.github.com>

--- examples/helpful_scripts/setup_feedback_iteration_gcp.py
+++ examples/helpful_scripts/setup_feedback_iteration_gcp.py
@@ -1,100 +1,22 @@
-# +
-from google.cloud import storage
-import os
 import yaml
-from datetime import datetime
 from pixaris.feedback_handlers.gcp import GCPFeedbackHandler
 
 config = yaml.safe_load(open("pixaris/config.yaml"))
 
-# -
-
-# ### function definitions
-
-
-def initialise_iteration_in_bigquery(
-    project: str,
-    feedback_iteration: str,
-    image_names: list[str],
-):
-    """
-    Initialise feedback iteration in BigQuery and upload images to GCP bucket.
-    """
-    gcp_feedback_handler = GCPFeedbackHandler(
-        gcp_project_id=config["gcp_project_id"],
-        gcp_bq_feedback_table=config["gcp_bq_feedback_table"],
-        gcp_pixaris_bucket_name=config["gcp_pixaris_bucket_name"],
-    )
-
-    # for each image, create the upload entry in feedback table
-    for image in image_names:
-        feedback = {
-            "project": project,
-            "feedback_iteration": feedback_iteration,
-            "image_name": image,
-            "feedback_indicator": "Neither",  # used only for initialisation of feedback iteration
-            "comment": "upload",
-        }
-        gcp_feedback_handler.write_single_feedback(feedback)
-
-
-def upload_images_to_bucket(
-    project: str,
-    feedback_iteration: str,
-    image_names: list[str],
-):
-    """
-    Upload images to GCP bucket.
-    """
-    storage_client = storage.Client(project=config["gcp_project_id"])
-    bucket = storage_client.bucket(config["gcp_pixaris_bucket_name"])
-
-    for filename in image_names:
-        if filename.endswith((".jpg", ".jpeg", ".png", ".tif")):
-            blob = bucket.blob(
-                f"results/{project}/feedback_iterations/{feedback_iteration}/{filename}"
-            )
-            blob.upload_from_filename(os.path.join(images_directory, filename))
-            print(f"Uploaded {filename} to {feedback_iteration}")
-
-
-def create_feedback_iteration(
-    images_directory: str,
-    project: str,
-    feedback_iteration: str,
-    date_suffix: str = None,
-):
-    """
-    Upload images to GCP bucket and persist initialisation of feedback iteration to BigQuery.
-
-    Args:
-        images_directory (str): Path to local directory containing images to upload.
-        feedback_iteration (str): Name of feedback iteration.
-        date_suffix (str): Suffix to add to feedback iteration
-    """
-    image_names = os.listdir(images_directory)
-
-    # add date for versioning if not provided
-    if not date_suffix:
-        date_suffix = datetime.now().strftime("%y%m%d")
-    feedback_iteration = f"{date_suffix}_{feedback_iteration}"
-
-    upload_images_to_bucket(
-        project=project, feedback_iteration=feedback_iteration, image_names=image_names
-    )
-    initialise_iteration_in_bigquery(
-        project=project, feedback_iteration=feedback_iteration, image_names=image_names
-    )
-
-
 # ### Create Feedback Iteration
+gcp_feedback_handler = GCPFeedbackHandler(
+    gcp_project_id=config["gcp_project_id"],
+    gcp_bq_feedback_table=config["gcp_bq_feedback_table"],
+    gcp_pixaris_bucket_name=config["gcp_pixaris_bucket_name"],
+)
+project = "dummy_project"
+feedback_iteration = "dummy_feedback_iteration"
+images_directory = "path_to_local_directory_containing_images"
 
-project = "name_of_project"
-feedback_iteration = "name_of_feedback_iteration"
-images_directory = "local_path_to_directory_with_images"
-
-create_feedback_iteration(
-    images_directory=images_directory,
+gcp_feedback_handler.create_feedback_iteration(
+    local_image_directory=images_directory,
     project=project,
     feedback_iteration=feedback_iteration,
+    dataset=None,  # optional
+    experiment_name=None,  # optional
 )
--- examples/helpful_scripts/setup_feedback_iteration_local.py
+++ examples/helpful_scripts/setup_feedback_iteration_local.py
@@ -1,66 +1,15 @@
-import os
-from datetime import datetime
 from pixaris.feedback_handlers.local import LocalFeedbackHandler
 
-
-def initialise_iteration_locally(
-    project: str,
-    feedback_iteration: str,
-    image_names: list[str],
-):
-    """
-    Initialise feedback iteration locally
-    """
-    local_feedback_handler = LocalFeedbackHandler()
-
-    # for each image, create the upload entry in feedback table
-    for image in image_names:
-        feedback = {
-            "project": project,
-            "feedback_iteration": feedback_iteration,
-            "image_name": image,
-            "feedback_indicator": "Neither",  # used only for initialisation of feedback iteration
-            "comment": "upload",
-        }
-        local_feedback_handler.write_single_feedback(feedback)
-
-
-def create_feedback_iteration(
-    images_directory: str,
-    project: str,
-    feedback_iteration: str,
-    date_suffix: str = None,
-):
-    """
-    Upload images to a folder and persist initialisation of feedback iteration to BigQuery.
-
-    Args:
-        images_directory (str): Path to local directory containing images to upload.
-        feedback_iteration (str): Name of feedback iteration.
-        date_suffix (str): Suffix to add to feedback iteration
-    """
-    image_names = os.listdir(images_directory)
-
-    # add date for versioning if not provided
-    if not date_suffix:
-        date_suffix = datetime.now().strftime("%y%m%d")
-    feedback_iteration = f"{date_suffix}_{feedback_iteration}"
-
-    initialise_iteration_locally(
-        project=project, feedback_iteration=feedback_iteration, image_names=image_names
-    )
-
-
 # ### Create Feedback Iteration
-
+feedback_handler = LocalFeedbackHandler()
 project = "dummy_project"
-feedback_iteration = "test_feedback_iteration_01"
-images_directory = (
-    "local_results/dummy_project/dummy_datatet/example-run_20250401-115520"
-)
+feedback_iteration = "dummy_feedback_iteration"
+local_image_directory = "path_to_local_directory_containing_images"
 
-create_feedback_iteration(
-    images_directory=images_directory,
+feedback_handler.create_feedback_iteration(
+    local_image_directory=local_image_directory,
     project=project,
     feedback_iteration=feedback_iteration,
+    dataset=None,  # optional
+    experiment_name=None,  # optional
 )
