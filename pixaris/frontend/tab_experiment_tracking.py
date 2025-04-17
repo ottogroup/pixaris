02e956478aafe61f3217c5e39608cea6d8363c3b
HenrikeMey <121957129+HenrikeMey@users.noreply.github.com>
2025-04-17 11:23:31 +0200

    fmt

--- pixaris/frontend/tab_experiment_tracking.py
+++ pixaris/frontend/tab_experiment_tracking.py
@@ -116,13 +116,11 @@ def render_experiment_tracking_tab(
                 return
             for experiment_name in experiments:
                 with gr.Accordion(label=f"Experiment {experiment_name}"):
-                    experiment_images = (
-                        experiment_handler.load_images_for_experiment(
-                            project=project_name,
-                            dataset=dataset,
-                            experiment_run_name=experiment_name,
-                            results_directory=results_directory,
-                        )
+                    experiment_images = experiment_handler.load_images_for_experiment(
+                        project=project_name,
+                        dataset=dataset,
+                        experiment_run_name=experiment_name,
+                        results_directory=results_directory,
                     )
                     gr.Gallery(
                         value=experiment_images,
