1d3379271653516f644470b219b0ad6a16169680
fideliusmanstein <fidelius.vonmanstein@ottogroup.com>
2025-04-29 09:28:41 +0200

    removed min, max, diff from saturation without mask

--- pixaris/metrics/saturation.py
+++ pixaris/metrics/saturation.py
@@ -72,27 +72,23 @@ class SaturationWithoutMaskMetric(BaseMetric):
 
     def _saturation(self, image: Image) -> float:
         """
-        Calculate the mean, variance, min, max and difference of the saturation of the image.
+        Calculate the mean and variance of the saturation of the image.
 
         :param image: The input image.
         :type image: Image.Image
-        :return: mean, variance, min, max and difference of the saturation
-        :rtype: tuple[float, float, float, float, float]
+        :return: mean and variance of the saturation
+        :rtype: tuple[float, float]
         """
         hue, saturation, brightness = image.convert("HSV").split()
         mean = np.mean(np.array(saturation))
         var = np.var(np.array(saturation))
-        min = np.min(np.array(saturation))
-        max = np.max(np.array(saturation))
-        diff = max - min
-        return (mean, var, min, max, diff)
+        return (mean, var)
 
     def calculate(self, generated_images: Iterable[Image]) -> dict:
         """
         Calculate the saturation score of a list of generated images.
-        For each image we calculate the mean, variance, min, max and difference of the saturation,
-        and return the average of them.
-        The result is 5 numbers between 0 and 1.
+        For each image we calculate the mean and variance of the saturation,
+        and return the average of them. The results is 2 numbers between 0 and 1
 
         :param generated_images: A list of generated images.
         :type generated_images: Iterable[Image]
@@ -105,10 +101,4 @@ class SaturationWithoutMaskMetric(BaseMetric):
             saturation_scores.append(brightness_difference)
 
         mean_values = np.mean(saturation_scores)
-        return {
-            "mean_saturation": mean_values[0],
-            "var_saturation": mean_values[1],
-            "min_saturation": mean_values[2],
-            "max_saturation": mean_values[3],
-            "difference_in_saturation": mean_values[4],
-        }
+        return {"saturation_mean": mean_values[0], "saturation_var": mean_values[1]}
