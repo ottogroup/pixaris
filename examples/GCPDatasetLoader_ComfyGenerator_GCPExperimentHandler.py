1e4607454f08b241d10767d2e78df2cf4e253cac
HenrikeMey <121957129+HenrikeMey@users.noreply.github.com>
2025-05-27 17:42:14 +0200

    avoid merge conflict

---
+++ examples/GCPDatasetLoader_ComfyGenerator_GCPExperimentHandler.py
@@ -0,0 +1,59 @@
+from PIL import Image
+from pixaris.data_loaders.gcp import GCPDatasetLoader
+from pixaris.experiment_handlers.gcp import GCPExperimentHandler
+from pixaris.generation.comfyui import ComfyGenerator
+from pixaris.orchestration.base import generate_images_based_on_dataset
+import os
+import yaml
+import json
+
+config = yaml.safe_load(open("pixaris/config.yaml", "r"))
+PROJECT = "dummy_project"
+DATASET = "dummy_dataset"
+with open(os.getcwd() + "/test/assets/test_inspo_apiformat.json", "r") as file:
+    WORKFLOW_APIFORMAT_JSON = json.load(file)
+WORKFLOW_PILLOW_IMAGE = Image.open(os.getcwd() + "/test/assets/test_inspo.png")
+EXPERIMENT_RUN_NAME = "example-run"
+
+
+# Setup the GCPDatasetLoader, ComfyGenerator, and GCPExperimentHandler
+data_loader = GCPDatasetLoader(
+    gcp_project_id=config["gcp_project_id"],
+    gcp_pixaris_bucket_name=config["gcp_pixaris_bucket_name"],
+    project=PROJECT,
+    dataset=DATASET,
+    eval_dir_local="local_experiment_inputs",
+)
+
+generator = ComfyGenerator(workflow_apiformat_json=WORKFLOW_APIFORMAT_JSON)
+
+experiment_handler = GCPExperimentHandler(
+    gcp_project_id=config["gcp_project_id"],
+    gcp_bq_experiment_dataset=config["gcp_bq_experiment_dataset"],
+    gcp_pixaris_bucket_name=config["gcp_pixaris_bucket_name"],
+)
+
+args = {
+    "workflow_apiformat_json": WORKFLOW_APIFORMAT_JSON,
+    "workflow_pillow_image": WORKFLOW_PILLOW_IMAGE,
+    "project": PROJECT,
+    "dataset": DATASET,
+    "pillow_images": [
+        {
+            "node_name": "Load Inspo Image",
+            "pillow_image": Image.open("test/assets/test_inspo_image.jpg"),
+        }
+    ],
+    "experiment_run_name": EXPERIMENT_RUN_NAME,
+}
+
+# execute
+out = generate_images_based_on_dataset(
+    data_loader=data_loader,
+    image_generator=generator,
+    experiment_handler=experiment_handler,
+    metrics=[],
+    args=args,
+)
+
+out[0][0].show()
