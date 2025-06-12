28a6275a39d008d0219e95ac9178c4f912e4b324
Theresa <theresa.wohlsen@ottogroup.com>
2025-06-12 16:50:59 +0200

    Add tests for utility modules

--- test/metrics/test_metric_utils.py
+++ test/metrics/test_metric_utils.py
@@ -18,6 +18,26 @@ class TestHelpers(unittest.TestCase):
         dicts = [{"a": 1, "b": 2}, {"a": 2, "c": 3}]
         self.assertRaises(ValueError, dict_mean, dicts)
 
+    def test_normalize_image_sizes_and_background(self):
+        from PIL import Image
+        from pixaris.metrics.utils import normalize_image
+
+        img = Image.new("RGB", (200, 100), "red")
+        result = normalize_image(img, max_size=(300, 300))
+
+        self.assertEqual(result.size, (300, 300))
+        self.assertEqual(result.getpixel((0, 0)), (255, 255, 255))
+        center = (150, 150)
+        self.assertEqual(result.getpixel(center), (255, 0, 0))
+
+    def test_normalize_image_downscale(self):
+        from PIL import Image
+        from pixaris.metrics.utils import normalize_image
+
+        img = Image.new("RGB", (600, 400), "blue")
+        result = normalize_image(img, max_size=(300, 300))
+        self.assertEqual(result.size, (300, 300))
+
     def test_dict_mean_different_lengths(self):
         dicts = [{"a": 1, "b": 2}, {"a": 2, "b": 2, "c": 3}]
         self.assertRaises(ValueError, dict_mean, dicts)
