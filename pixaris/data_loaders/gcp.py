c164ac19a6253ca63a62b912572062ea8a6ff3fa
Theresa <theresa.wohlsen@ottogroup.com>
2025-06-12 16:49:20 +0200

    refactor: share dataset loader helpers

--- pixaris/data_loaders/gcp.py
+++ pixaris/data_loaders/gcp.py
@@ -138,34 +138,6 @@ class GCPDatasetLoader(DatasetLoader):
                     )
                 )
 
-    def _retrieve_and_check_dataset_image_names(self):
-        """
-        Retrieves the names of the images in the evaluation set and checks if they are the same in each image directory.
-
-        :return: The names of the images in the evaluation set.
-        :rtype: List[str]
-        :raises: ValueError: If the names of the images in each image directory are not the same.
-        """
-        basis_names = os.listdir(
-            os.path.join(
-                self.eval_dir_local, self.project, self.dataset, self.image_dirs[0]
-            )
-        )
-        basis_names = [name for name in basis_names if name != ".DS_Store"]
-        for image_dir in self.image_dirs:
-            image_names = os.listdir(
-                os.path.join(self.eval_dir_local, self.project, self.dataset, image_dir)
-            )
-            image_names = [name for name in image_names if name != ".DS_Store"]
-
-            if basis_names != image_names:
-                raise ValueError(
-                    "The names of the images in each image directory should be the same. {} does not match {}.".format(
-                        self.image_dirs[0], image_dir
-                    )
-                )
-        return basis_names
-
     def load_dataset(
         self,
     ) -> List[dict[str, List[dict[str, Image.Image]]]]:
@@ -179,26 +151,8 @@ class GCPDatasetLoader(DatasetLoader):
         :rtype: List[dict[str, List[dict[str, Image.Image]]]]:
         """
         self._download_dataset()
-        image_names = self._retrieve_and_check_dataset_image_names()
-
-        dataset = []
-        for image_name in image_names:
-            pillow_images = []
-            for image_dir in self.image_dirs:
-                image_path = os.path.join(
-                    self.eval_dir_local,
-                    self.project,
-                    self.dataset,
-                    image_dir,
-                    image_name,
-                )
-                # Load the image using PIL
-                pillow_image = Image.open(image_path)
-                pillow_images.append(
-                    {
-                        "node_name": f"Load {image_dir.capitalize()} Image",
-                        "pillow_image": pillow_image,
-                    }
-                )
-            dataset.append({"pillow_images": pillow_images})
-        return dataset
+        dataset_dir = os.path.join(self.eval_dir_local, self.project, self.dataset)
+        image_names = self._retrieve_and_check_dataset_image_names(
+            dataset_dir, self.image_dirs
+        )
+        return self._assemble_dataset(dataset_dir, self.image_dirs, image_names)
--- pixaris/data_loaders/local.py
+++ pixaris/data_loaders/local.py
@@ -47,66 +47,12 @@ class LocalDatasetLoader(DatasetLoader):
                 f"Please ensure the dataset contains subdirectories with images."
             )
 
-    def _retrieve_and_check_dataset_image_names(self):
-        """
-        Retrieves the names of the images in the evaluation set and checks if they are the same in each image directory.
-
-        :raises ValueError: If the names of the images in each image directory are not the same.
-        :return: The names of the images in the evaluation set.
-        :rtype: list[str]
-        """
-        basis_names = os.listdir(
-            os.path.join(
-                self.eval_dir_local, self.project, self.dataset, self.image_dirs[0]
-            )
-        )
-        basis_names = [name for name in basis_names if name != ".DS_Store"]
-        for image_dir in self.image_dirs:
-            image_names = os.listdir(
-                os.path.join(self.eval_dir_local, self.project, self.dataset, image_dir)
-            )
-            image_names = [name for name in image_names if name != ".DS_Store"]
-
-            if basis_names != image_names:
-                raise ValueError(
-                    "The names of the images in each image directory should be the same. {} does not match {}.".format(
-                        self.image_dirs[0], image_dir
-                    )
-                )
-        return basis_names
-
     def load_dataset(
         self,
     ) -> List[dict[str, List[dict[str, Image.Image]]]]:
-        """
-        Returns all images in the evaluation set as an iterable of dictionaries containing PIL Images.
-
-        :return: list of dicts containing data loaded from the local directory.
-            The key will always be "pillow_images".
-            The value is a dict mapping node names to PIL Image objects.
-            This dict has a key for each directory in the image_dirs list representing a Node Name.
-        :rtype: List[dict[str, List[dict[str, Image.Image]]]]:
-        """
-        image_names = self._retrieve_and_check_dataset_image_names()
-
-        dataset = []
-        for image_name in image_names:
-            pillow_images = []
-            for image_dir in self.image_dirs:
-                image_path = os.path.join(
-                    self.eval_dir_local,
-                    self.project,
-                    self.dataset,
-                    image_dir,
-                    image_name,
-                )
-                # Load the image using PIL
-                pillow_image = Image.open(image_path)
-                pillow_images.append(
-                    {
-                        "node_name": f"Load {image_dir.capitalize()} Image",
-                        "pillow_image": pillow_image,
-                    }
-                )
-            dataset.append({"pillow_images": pillow_images})
-        return dataset
+        """Return all images for this dataset."""
+        dataset_dir = os.path.join(self.eval_dir_local, self.project, self.dataset)
+        image_names = self._retrieve_and_check_dataset_image_names(
+            dataset_dir, self.image_dirs
+        )
+        return self._assemble_dataset(dataset_dir, self.image_dirs, image_names)
